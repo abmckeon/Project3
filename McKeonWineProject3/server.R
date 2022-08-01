e8d7ab546fc40b45527870355b8c9a4aee8ab740
Ashlee McKeon <mckeon.ashlee@gmail.com>
2022-08-01 18:45:58 -0400

    Final Commit

--- McKeonWineProject3/server.R
+++ McKeonWineProject3/server.R
@@ -4,31 +4,93 @@ shinyServer(function(input, output, session) {
   output$myrandom <- renderUI(
     plotOutput("id_data")
   )
-  data_filtered <- reactive({
+  explore_filtered <- reactive({
     wineMag %>%
-      filter(`score` < input$id_slider1[2],
-             `score` > input$id_slider1[1],
-             `price` < input$id_slider2[2],
-             `price` > input$id_slider2[1],
+      filter(`points` <= input$id_slider1[2],
+             `points` >= input$id_slider1[1],
+             `price` <= input$id_slider2[2],
+             `price` >= input$id_slider2[1],
              `country` == input$id_radio)
-  })
-  
+    })
+   
   wholeData <- reactive({
     wineMag})
   
-  output$id_table <- renderTable(data_filtered())
-
-  #output$id_explore <- renderPlot(
-    #explore_filtered()%>%
-      #ggplot(aes(`explore`))+
-      #geom_histogram()+
-      #theme_solarized())
+  output$id_data <- renderPlot(
+    if (input$change_plot == "bar") {
+      explore_filtered() %>%
+      ggplot(aes_string(x= 'province', y='price', fill='province')) +
+        geom_bar(stat = "identity")+
+        theme(aspect.ratio=1)
+    } else {
+      explore_filtered() %>%
+      ggplot(aes_string(x = "", y='price', fill = 'province')) +
+        geom_bar(stat = "identity", width = 1) +
+        coord_polar("y", start=0)
+      }
+    )
   
-  #output$id_model <- renderPlot(
-    #model_filtered()%>%
-      #ggplot(aes(`model`))+
-      #geom_histogram()+
-      #theme_solarized())
+#   output$id_models <- observeEvent(input$do, input$id_numeric, input$id_x1, input$id_x2 {
+#     set.seed(1106)
+#     trainIndex <- createDataPartition(wineMag, input$id_numeric, list = FALSE)
+#     wineMagTrain <- wineMag[trainIndex, ]
+#     wineMagTest <- wineMag[-trainIndex, ]
+# 
+#     ## set up trainControl using 5-fold cross validation
+#     trnCntrl <- trainControl(method = "cv", number=5)
+# 
+#     ## NewtrnCntrl for ENSEMBLE METHODS
+#     NewtrnCntrl <- trainControl(method = "cv", number=5, repeats=3)
+#     
+#     glmFit <- train(wineMag$price ~ (input$id_x1, input$id_x2,
+#                               data = wineMagTrain,
+#                               method = "glm",
+#                               preProcess = c("center", "scale"),
+#                               trControl = trnCntrl))
+# 
+#     rfFit <- train(wineMag$price ~ input$id_x1, input$id_x2,
+#                    data = wineMagTrain,
+#                    method = "rf",
+#                    trControl = NewtrnCntrl,
+#                    preProcess = c("center", "scale"),
+#                    tuneGrid = data.frame(mtry= 1:7))
+# 
+#     gbmGrid <-  expand.grid(interaction.depth = c(1,2,3,4),
+#                             n.trees = c(25,50,100,150,200),
+#                             shrinkage = 0.1,
+#                             n.minobsinnode = 10)
+# 
+#     boostFit <- train(wineMag$price ~ input$id_x1, input$id_x2,
+#                       data = wineMagTrain,
+#                       method = "gbm",
+#                       trControl = NewtrnCntrl,
+#                       preProcess = c("center", "scale"),
+#                       tuneGrid = gbmGrid,
+#                       verbose = FALSE)
+# 
+#     #Compare model performance on training set and print summaries
+#     rmse(glmFit, wineMagTrain)
+#     summary(glmFit)
+# 
+#     rfFit.pred <- predict(rfFit, newdata = wineMagTrain)
+#     print(rfFit.pred)
+# 
+#     rmse(boostFit, wineMagTrain)
+#     print(boostFitPred)
+#     
+#     #Apply models to test set
+#     rmse(glmFit, wineMagTest)
+#     summary(glmFit)
+#     
+#     rfFit.pred <- predict(rfFit, wineMagTest)
+#     print(rfFit.pred)
+#     
+#     boostFit.pred <- predict(boostFitFit, wineMagTest)
+#     print(boostFit.pred)
+# })
+# 
+
+  output$id_table <- renderTable(explore_filtered())
   
   output$wineMag <- renderDataTable(wholeData())
   
--- McKeonWineProject3/ui.R
+++ McKeonWineProject3/ui.R
@@ -3,8 +3,9 @@ library(tidyverse)
 library(shinythemes)
 library(shinyWidgets)
 library(ggthemes)
+library(mathjaxr)
 
-wineMag <- read_csv("/Users/ashleebrookemckeon/Desktop/ST558_Databases/Project3/wineMag.csv") 
+wineMag <- read_csv("/Users/ashleebrookemckeon/Desktop/ST558_Databases/Project3/McKeonWineProject3/wineMag.csv") 
                            col_types = cols(`id` = col_double(),
                                             `country` = col_character(), 
                                             `description` = col_character(), 
@@ -34,48 +35,119 @@ shinyUI(fluidPage(theme = shinytheme("sandstone"),
       margin: 0;
       margin-bottom: 1rem;
       padding: 0;
-      width: 100%;
+      width: 50%;
     }"
                                    )
                               )
                    ),
 
     # Application title
-    titlePanel("Wine Enthusiast Wine Reviews"),
+    titlePanel("WineEnthusiast Wine Reviews"),
 
     sidebarLayout(
-      sidebarPanel(sliderInput("id_slider1", "Select Score Range",
-                               min = 0, max = 100, value = c(50, 70), pre = "%"),
+      sidebarPanel(sliderInput("id_slider1", "Select Point Range",
+                               min = 0, max = 100, value = c(90, 95), post = "%"),
                    
                    sliderInput("id_slider2", "Select Price Range",
                                min = 0, max = 500, value = c(100, 150), pre = "$"),
                    
-                   radioGroupButtons("id_radio", "Select Country", 
+                   radioGroupButtons("id_radio", "Select Country",
                                      choices= unique(wineMag$`country`),
-                                     status = "success")
+                                     status = "success"),
+                   selectInput("id_numeric", "Select the % of the full dataset to be used for the training set. Note: The remainder will be automatically used for the test set to ensure the test and training sets add to 1 or 100% of the full dataset.",
+                               choices = c(0.25, 0.50, 0.75)),  
+                   selectInput("id_x1", "Select Predictor Variable 1:",
+                               choices = list("price", "country", "province"),
+                               selected = "price"),
+                   selectInput("id_x2", "Select Predictor Variable 2:",
+                               choices = list("price", "country", "province"),
+                               selected = "price")
                    ),
     
       mainPanel(tabsetPanel 
                 (uiOutput("myrandom"),
                   
-                  tabPanel("About",h5(div(p("The purpose of this app is to explore a dataset containing ~130k wine reviews published to WineEnthusiast."),
+                  tabPanel("About",h4(div(p("The purpose of this app is to explore a dataset containing ~130k wine reviews published to WineEnthusiast."),
                                           br(),
                                           p("The dataset comes from Kaggle and contains information on taster name, wine price, variety, score, country, county, vineyard, etc."),
                                           br(),
-                                          p("You can visit this link for more informaton about the dataset [WineEnthusiast] < https://www.kaggle.com/datasets/mysarahmadbhat/wine-tasting>."),
+                                          p(tags$a(href="https://www.kaggle.com/datasets/mysarahmadbhat/wine-tasting", "Click here for more information about the dataset!")),
                                           br(),
                                           p("The sliders on the right control the rating and price of the wine and the buttons control the country the wine is produced in. "),
                                           br(),
                                           p("Below is an image of the cover of the famous Wine Enthusiast Magazine."),
                                           br(),
-                                          img(src = "wineMagCover.jpeg"),
-                                          br(),
                                           p("This tab tells you information about the data. The 'Data Exploraton' tab displays graphical and numeric summaries based on user input. The 'Modeling' tab fits 3 supervised models to the data. Finally, the 'Data' tab allows you to scroll through, subset, and download the data.  Click one of the tabs to begin."),
                                           br(),
+                                          img(src = "wineMagCover.jpeg"),
                                           )
                                       )
                            ),
                   
+                  tabPanel("Data Exploration",
+                           br(),
+                           h4(p("Here you can use the sliders and radio buttons to sort through the data by points, price, and country to create a different numerical summaries.")),
+                           br(),
+                           tableOutput(outputId="id_table"),
+                           br(),
+                           h4(p("With this scatterplot, you can visually examine the way the direction and strength of the relationship between points and price of wines changes as a function of the country the wine is from.")),
+                           br(),
+                           radioGroupButtons("change_plot", "Select a Visualization",
+                             choices = c(
+                               `<i class='fa fa-bar-chart'></i>` = "bar",
+                               `<i class='fa fa-pie-chart'></i>` = "pie"
+                             ),
+                             justified = TRUE,
+                             selected = "bar"
+                             ),
+                           br(),
+                           plotOutput(outputId="id_data", height = 250)
+                           ),
+                  
+                  tabPanel ("Modeling",
+                            br(),
+                            h4(p("Please click the 3 tabs to learn more about the modeling process used on the wineMag dataset.")),
+                            
+                            tabsetPanel(
+                              tabPanel("Modeling Info",h4(div(p("Three models have been run on the wineMag data. Those models, their advantages and drawbacks are described below."),
+                                                              br(),
+                                                              h4(p("A multiple linear regression model models the linear relationship between a continuous response variable and two or more continuous predictors. This model is based on, and expanded from, the simpliest regression model individuals typically learned in beginners classes on of:")),
+                                                              title = 'MathJax Examples',
+                                                              withMathJax(),
+                                                              helpText('$$y=mx+b$$'),
+                                                              br(),
+                                                              h4(p("Advantages of this model include the ability to determine the relative predictvie contributions of each predictor to the model and the ability to identify outliers. Disadvantages of this model include, but are not limited to, the need for continous variables and the potential for underfitting or the senstivity of the model to outliers.")),
+                                                              br(),
+                                                              h4(p("A boosted tree model is an ensemble method that models a response variable using multiple models and when combining those models into one combined model for greater prediction power. The booted tree model models tree sequentially. Advantages of this model include the enhanced prediction processing capability, when using a capable system and the increased predictive power compared to non-ensemble methods. Disadvantages of this model include, but are not limited to, the senstivity of the model to outliers and the computing power needed.")),
+                                                              br(),
+                                                              h4(p("A random forest model is another ensemble method that trains many decision trees at one time and then combines those training models into one combined model for testing on a test dataset for greater prediction power. Advantages of this model include the versatility for being used for both regression and classification, as well as decreased fear of overfitting your model as long as you have enough trees to work with. Disadvantages of this model include, but are not limited to, the computing power needed can make running these models quite slow and it also many be complex to interpret to those unfamilar with the method.")),
+                                                              br(),
+                              )
+                              )
+                              ),
+                              
+                              tabPanel("Model Fitting",h4(div(p("Model fitting results are below."),
+                                                              br(),
+                                                              tags$head(tags$script(src = "message-handler.js")),
+                                                              actionButton("do", "Click to Run All Models", icon=icon("play")),
+                                                              br(),
+                                                              verbatimTextOutput("output$id_models")
+                              )
+                              )
+                              ),
+                              
+                              tabPanel("Prediction",h4(div(p("Here you can use Model X for your own predictions. Select the values for the predictor variables below and see the prediction change"),
+                                                           br(),
+                                                           #plotOutput(outputId="id_model")
+                              )
+                              )
+                              ),
+                              
+                            )
+                            
+
+                  ),
+                            
                   tabPanel("Data",
                            br(),
                            h5(p("View the entire dataset and download here")),
@@ -87,47 +159,11 @@ shinyUI(fluidPage(theme = shinytheme("sandstone"),
                                         block = FALSE,
                                         no_outline = TRUE),
                            br(),
-
                            dataTableOutput("wineMag"),
                            br()
-                           )
+                           ),
                   )
                 )
       )
     )
     )
-
-                
-                  #tabPanel("Data Exploration", 
-                           #br(),
-                           #h4(p("Here you can use the sliders and radio buttons to sort through the data by score, price, and country.")),
-                           #br(),
-                           #tableOutput(outputId="id_table")),
-                           #h4(p("With this histogram, you can sort through the data by looking at the distribution of countries as a function of score and price.")), 
-                           #br(),
-                           #plotOutput(outputId="id_explore")),
-                  
-                  #tabPanel ("Modeling",
-                            #br(),
-                            #h4(p("Please click the 3 tabs to learn more about the modeling process used on the wineMag dataset."))),
-                            
-                            #tabPanel("Modeling Info",h3(div(p("Three models have been run on the wineMag data. Those models, their advantages and drawbacks are described below."),
-                                                            #br(),
-                                                            #p("Model 1: "),
-                                                            #br(),
-                                                            #p("Model 2: "),
-                                                            #br(),
-                                                            #br(),
-                                                            #p("Model 3: ")
-                                                            #))),
-                                                            
-                            #tabPanel("Model Fitting",h4(div(p("Model fitting results are below."),
-                                                            #br()
-                                                            #))),
-                                                            
-                            #tabPanel("Prediction",h4(div(p("Here you can use Model X for your own predictions. Select the values for the predictor variables below and see the prediction change"),
-                                                            #br(),
-                            #br(),
-                            #plotOutput(outputId="id_model")),
-                  
-
